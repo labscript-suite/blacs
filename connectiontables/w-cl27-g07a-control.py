130748a2cda60d3f8141a0090e922b76fe752d9f
Russell Anderson <5637107+rpanderson@users.noreply.github.com>
2011-11-18 01:56:59 +0000

    Updated connection table in G07a.

--- connectiontables/w-cl27-g07a-control.py
+++ connectiontables/w-cl27-g07a-control.py
@@ -20,6 +20,7 @@ AnalogIn( 'quad_current',  ni_pcie_6363_0,         'ai3')
 
 # ZEEMAN SLOWER
 DigitalOut('Zeeman_coils',  ni_pcie_6363_0,  'port0/line12')
+DigitalOut('Zeeman_shutter',ni_pcie_6363_0,  'port0/line13')
 
 # SHUTTERS
 Shutter(    'MOT_shutter',  ni_pcie_6363_0, 'port0/line0', delay=(0,0))
@@ -28,6 +29,9 @@ Shutter('Imaging_shutter',  ni_pcie_6363_0, 'port0/line2', delay=(0,0))
 Shutter( 'Repump_shutter',  ni_pcie_6363_0, 'port0/line3', delay=(0,0))
 Shutter(     'OP_shutter',  ni_pcie_6363_0, 'port0/line4', delay=(0,0))
 
+# CAMERA
+DigitalOut('Camera_trigger',ni_pcie_6363_0,  'port0/line14')
+
 # POWER MONITORING
 AnalogIn(    'MOT_power',  ni_pcie_6363_0,         'ai4')
 AnalogIn( 'Zeeman_power',  ni_pcie_6363_0,         'ai5')
@@ -35,20 +39,29 @@ AnalogIn(     'OP_power',  ni_pcie_6363_0,         'ai6')
 AnalogIn('Imaging_power',  ni_pcie_6363_0,         'ai7')
 AnalogIn( 'TA_seed_leak',  ni_pcie_6363_0,         'ai8')
 
+# FLUORESCENCE MONITORING
+AnalogIn( 'MOT_fluoro',  ni_pcie_6363_0,         'ai9')
+
 # SUPERNOVA
 DDS(                 'MOPA',  novatechdds9m_9,   'channel 0')
-DDS(                   'RF',  novatechdds9m_9,   'channel 1')
-StaticDDS(         'Zeeman',  novatechdds9m_9,   'channel 2')
-StaticDDS(        'Imaging',  novatechdds9m_9,   'channel 3')
+DDS(               'Zeeman',  novatechdds9m_9,   'channel 1') # temporary assignment
+StaticDDS(            'MOT',  novatechdds9m_9,   'channel 2')
+StaticDDS(        'Imaging',  novatechdds9m_9,   'channel 3') # temporary assignment
 DigitalOut(   'MOPA_enable',  ni_pcie_6363_0,  'port0/line5')
-DigitalOut(     'RF_enable',  ni_pcie_6363_0,  'port0/line6')
-DigitalOut( 'Zeeman_enable',  ni_pcie_6363_0,  'port0/line7')
-DigitalOut('Imaging_enable',  ni_pcie_6363_0,  'port0/line8')
+DigitalOut( 'Zeeman_enable',  ni_pcie_6363_0,  'port0/line6') # temporary assignment
+DigitalOut(    'MOT_enable',  ni_pcie_6363_0,  'port0/line7')
+DigitalOut('Imaging_enable',  ni_pcie_6363_0,  'port0/line8') # temporary assignment
+# DDS(                  'RF',  novatechdds9m_9,   'channel 1') forseen assignment
+# StaticDDS(        'Repump',  novatechdds9m_9,   'channel 3') forseen assignment
+# DigitalOut(     'RF_enable',  ni_pcie_6363_0,  'port0/line6')  forseen assignment
+# DigitalOut( 'Repump_enable',  ni_pcie_6363_0,  'port0/line8')  forseen assignment
+
+
 
 # PULSEBLASTER 0 DDS
-DDS(                 'MOT',  pulseblaster_0,       'dds 0')
-DDS(              'Repump',  pulseblaster_0,       'dds 1')
-DigitalOut(   'MOT_enable',  ni_pcie_6363_0,  'port0/line9')
-DigitalOut('Repump_enable',  ni_pcie_6363_0,  'port0/line10')
+# DDS(               'Zeeman',  pulseblaster_0,       'dds 0')   forseen assignment
+# DDS(              'Imaging',  pulseblaster_0,       'dds 1')   forseen assignment
+# DigitalOut( 'Zeeman_enable',  ni_pcie_6363_0,  'port0/line9')  forseen assignment
+# DigitalOut('Imaging_enable',  ni_pcie_6363_0,  'port0/line10') forseen assignment
 
 stop(0)
