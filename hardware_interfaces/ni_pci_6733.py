52307847ee3436a05cd6b76da6abb846d2c992ba
Chris Billington <chrisjbillington@gmail.com>
2012-01-18 04:43:51 +0000

    The 6733 should now update the front panel with the final state of the run, and should reprogram the initial state upon a run being aborted. Requires testing.

--- hardware_interfaces/ni_pci_6733.py
+++ hardware_interfaces/ni_pci_6733.py
@@ -61,6 +61,7 @@ class ni_pci_6733(Tab):
 #            self.digital_outs.append(output)
             
         self.analog_outputs = []
+        self.analog_outputs_by_channel = {}
         for i in range(self.num_AO):
             # Get the widgets:
             spinbutton = self.builder.get_object("AO_value_%d"%(i+1))
@@ -84,6 +85,7 @@ class ni_pci_6733(Tab):
             
             output = AO(name, channel,spinbutton, combobox, calib, calib_params, def_calib_params, self.program_static, self.min_ao_voltage, self.max_ao_voltage, self.ao_voltage_step)
             self.analog_outputs.append(output)
+            self.analog_outputs_by_channel[channel] = output
             
         self.viewport.add(self.toplevel)
         self.initialise_device()
@@ -116,31 +118,29 @@ class ni_pci_6733(Tab):
     @define_state
     def transition_to_buffered(self,h5file,notify_queue):
         self.static_mode = False      
-        # The initial values, to program back in in case of an abort:
-        self.initial_values = [ouput.value for output in self.analog_outputs]         
         self.queue_work('program_buffered',h5file)
         self.do_after('leave_program_buffered',notify_queue)
     
     def leave_program_buffered(self,notify_queue,_results):
+        # The final values of the run, to update the GUI with at the end of the run:
+        self.final_values = _results
         # Tell the queue manager that we're done:
         notify_queue.put(self.device_name)
         
     @define_state
     def abort_buffered(self):        
-        self.queue_work('abort_buffered')
-        self.do_after('leave_transition_to_static',notify_queue=None)
+        self.queue_work('transition_to_static',abort=True)
         
     @define_state        
     def transition_to_static(self,notify_queue):
-        self.queue_work('transition_to_static')
-        self.do_after('leave_transition_to_static',notify_queue)
-        
-    def leave_transition_to_static(self,notify_queue,_results):    
         self.static_mode = True
-        # Tell the queue manager that we're done:
-        if notify_queue is not None:
-            notify_queue.put(self.device_name)
-    
+        self.queue_work('transition_to_static')
+        # Tell the queue manager we're done:
+        notify_queue.put(self.device_name)
+        # Update the GUI with the final values of the run:
+        for channel, value in self.final_values:
+            self.analog_outputs_by_channel[chanel].set_value(value,program=False)
+            
     def get_child(self,type,channel):
         if type == "DO":
             if channel >= 0 and channel < self.num_DO:
@@ -154,17 +154,13 @@ class ni_pci_6733(Tab):
     
     
 class NiPCI6733Worker(Worker):
+
+    num_AO = 8
+    
     def init(self):
-        
         exec 'from PyDAQmx import Task, DAQmxConnectTerms, DAQmxDisconnectTerms' in globals()
         exec 'from PyDAQmx.DAQmxConstants import *' in globals()
         exec 'from PyDAQmx.DAQmxTypes import *' in globals()
-        
-        global ni_programming; from hardware_programming import ni_pci_6733 as ni_programming
-        self.num_DO = 0
-        self.num_AO = 8
-        self.num_RF = 0
-        self.num_AI = 0     
     
     def initialise(self, device_name, limits):
         # Create task
@@ -191,17 +187,47 @@ class NiPCI6733Worker(Worker):
         self.ao_data[:] = analog_data
         self.ao_task.WriteAnalogF64(1,True,1,DAQmx_Val_GroupByChannel,self.ao_data,byref(self.ao_read),None)
           
-    def program_buffered(self,h5file):        
-        self.ao_task = ni_programming.program_buffered_output(h5file,self.device_name,self.ao_task)
-        # require final values here to be returned
+    def program_buffered(self,h5file):  
+        with h5py.File(h5file,'r') as hdf5_file:
+            group = hdf5_file['devices/'][self.device_name]
+            clock_terminal = group.attrs['clock_terminal']
+            h5_data = group.get('ANALOG_OUTS')
+            if h5_data:
+                ao_channels = group.attrs['analog_out_channels']
+                # We use all but the last sample (which is identical to the
+                # second last sample) in order to ensure there is one more
+                # clock tick than there are samples. The 6733 requires this
+                # to determine that the task has completed.
+                ao_data = pylab.array(h5_data,dtype=float64)[:-1,:]
+                
+                self.ao_task.StopTask()
+                self.ao_task.ClearTask()
+                self.ao_task = Task()
+                ao_read = int32()
+
+                self.ao_task.CreateAOVoltageChan(ao_channels,"",-10.0,10.0,DAQmx_Val_Volts,None)
+                self.ao_task.CfgSampClkTiming(clock_terminal,1000000,DAQmx_Val_Rising,DAQmx_Val_FiniteSamps, ao_data.shape[0])
+                
+                self.ao_task.WriteAnalogF64(ao_data.shape[0],False,10.0,DAQmx_Val_GroupByScanNumber, ao_data,ao_read,None)
+                self.ao_task.StartTask()   
+                
+                # Final values here are a dictionary of values, keyed by channel:
+                final_values = {channel.split('/')[1]: value for channel, value in zip(ao_channels, ao_data[-1,:])}
+                return final_values
+            else:
+                return {}
         
     def transition_to_static(self,abort=False):
         if not abort:
-            # if aborting, don't call StopTask since this thorws an
+            # if aborting, don't call StopTask since this throws an
             # error if the task hasn't actually finished!
             self.ao_task.StopTask()
         self.ao_task.ClearTask()
         self.ao_task = Task()
         self.setup_static_channels()
         self.ao_task.StartTask()
+        if abort:
+            # Reprogram the initial states:
+            self.program_static(self.ao_data)
+        
         
