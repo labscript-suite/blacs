2893041cdb93aadf96f09c10118fc9f16750a822
Chris Billington <chrisjbillington@gmail.com>
2012-04-26 05:21:17 +0000

    Made a change to raise exceptions during the NI_6363's abort function rather than ignore them.

--- hardware_interfaces/ni_pcie_6363.py
+++ hardware_interfaces/ni_pcie_6363.py
@@ -362,8 +362,7 @@ class NiPCIe6363Worker(Worker):
         self.to_child.put(["transition to static",(self.device_name,abort)])
         result,message = self.from_child.get()
         if result == 'error':
-            if not abort: # Ignore the error if the run was aborted:
-                raise Exception(message)
+            raise Exception(message)
         
 # Worker class for AI input:
 class Worker2(multiprocessing.Process):
