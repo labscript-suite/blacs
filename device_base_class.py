f98230df6a2b3d91ad01d4bb8c6022abce48dae6
Phil Starkey <philipstarkey@users.noreply.github.com>
2013-02-17 18:04:36 +1100

    Renamed STATE_MANUAL, etc to MODE_MANUAL, etc.
    Added functionality for checking "remote" values and creating a dialog asking the user to choose which values to use!

--- device_base_class.py
+++ device_base_class.py
@@ -2,9 +2,10 @@ import sys
 
 from PySide.QtCore import *
 from PySide.QtGui import *
+from PySide.QtUiTools import QUiLoader
 
 from tab_base_classes import Tab, Worker, define_state
-from tab_base_classes import STATE_MANUAL, STATE_TRANSITION_TO_BUFFERED, STATE_TRANSITION_TO_MANUAL, STATE_BUFFERED  
+from tab_base_classes import MODE_MANUAL, MODE_TRANSITION_TO_BUFFERED, MODE_TRANSITION_TO_MANUAL, MODE_BUFFERED  
 from output_classes import AO, DO, DDS
 from qtutils.widgets.toolpalette import ToolPaletteGroup
 
@@ -22,24 +23,27 @@ class DeviceTab(Tab):
         
         self._final_values = {}
         self._last_programmed_values = {}
+        self._last_remote_values = {}
         self._primary_worker = None
         self._can_check_remote_values = False
-        self._check_remote_value_id = None
+        self._changed_radio_buttons = {}
         
         # Call the initialise GUI function
-        self.initialise_GUI()
-        self.initialise_device()
-        
+        self.initialise_GUI()        
+        self.initialise_device()        
+        self._last_programmed_values = self.get_front_panel_values()
+        if self._can_check_remote_values:
+            self.statemachine_timeout_add(30000,self.check_remote_values)
     
     def initialise_GUI(self):
         # Override this function
         # set the primary worker at this time
         pass
     
-    @define_state(STATE_MANUAL,True)
+    @define_state(MODE_MANUAL,True)
     def initialise_device(self):
-        self._last_programmed_values = self.get_front_panel_values()
         pass
+        
         
     @property
     def primary_worker(self):
@@ -51,10 +55,6 @@ class DeviceTab(Tab):
     
     def supports_remote_value_check(self,support)
         self._can_check_remote_values = bool(support)
-        if self._can_check_remote_values:
-            self._check_remote_value_id = self.statemachine_timeout_add(30,self.check_remote_values)
-        else:
-            self.statemachine_timeout_remove(self._check_remote_value_id)
     
     ############################################################
     # What do the properties dictionaries need to look like?   #
@@ -136,7 +136,7 @@ class DeviceTab(Tab):
     def create_analog_outputs(self,analog_properties):
         for hardware_name,properties in analog_properties.items():                    
             # Create and save the AO object
-            self._AO[hardware_name] = self._create_AO_object(self._device_name,hardware_name,properties)
+            self._AO[hardware_name] = self._create_AO_object(self.device_name,hardware_name,properties)
 
     def _create_AO_object(self,parent_device,hardware_name,properties):
         # Find the connection name
@@ -152,11 +152,14 @@ class DeviceTab(Tab):
             calib_params = eval(device.unit_conversion_params)
         
         # Instantiate the AO object
-        return AO(hardware_name, connection_name, self._device_name, self.program_device, self._settings, calib_class, calib_params,
+        return AO(hardware_name, connection_name, self.device_name, self.program_device, self._settings, calib_class, calib_params,
                 properties['base_unit'], properties['min'], properties['max'], properties['step'], properties['decimals'])
             
     def create_dds_outputs(self,dds_properties):
         for hardware_name,properties in dds_properties.items():
+            device = self._connection_table.find_child(self.device_name,hardware_name)
+            connection_name = device.name if device else '-'
+        
             subchnl_name_list = ['freq','amp','phase']
             sub_chnls = {}
             for subchnl in subchnl_name_list:
@@ -167,7 +170,7 @@ class DeviceTab(Tab):
             if 'gate' in properties:
                 sub_chnls['gate'] = self._create_DO_object(hardware_name,hardware_name+'_gate',properties)
             
-            self._DDS[hardware_name] = DDS(sub_chnls)
+            self._DDS[hardware_name] = DDS(hardware_name,connection_name,sub_chnls)
         
     def create_digital_widgets(self,channel_properties):
         widgets = {}
@@ -256,71 +259,174 @@ class DeviceTab(Tab):
         else:
             return None
             
-    @define_state(STATE_MANUAL|STATE_BUFFERED|STATE_TRANSITION_TO_BUFFERED|STATE_TRANSITION_TO_MANUAL,True)
+    @define_state(MODE_MANUAL|MODE_BUFFERED|MODE_TRANSITION_TO_BUFFERED|MODE_TRANSITION_TO_MANUAL,True)
     def destroy(self):
         self.queue_work('shutdown')
         self.destroy_complete = True
         self.close_tab()
     
-    # Only allow this to be called when we are in STATE_MANUAL and keep it queued up if we are not
-    @define_state(STATE_MANUAL,True)
+    # Only allow this to be called when we are in MODE_MANUAL and keep it queued up if we are not
+    # When pulling out the state from the state queue, we check to see if there is an adjacent state that is more recent, and use that one
+    # or whichever is the latest without encountering a different state).
+    # This prevenets 'a million' calls to program_device from executing, potentially slowing down the system
+    @define_state(MODE_MANUAL,True,delete_stale_states=True)
     def program_device(self):
         self._last_programmed_values = self.get_front_panel_values()
-        yield(self.queue_work(self._primary_worker,'program_manual',self._last_programmed_values))
+        
+        # get rid of any "remote values changed" dialog
+        self._changed_widget.hide()
+        
+        results = yield(self.queue_work(self._primary_worker,'program_manual',self._last_programmed_values))
+        
+        # If the worker process returns something, we assume it wants us to coerce the front panel values
+        if results:
+            for channel,remote_value in results.items():
+                if channel not in self._last_programmed_values:
+                    raise RuntimeError('The worker function program_manual for device %s is returning data for channel %s but the BLACS tab is not programmed to handle this channel'%(self.device_name,channel))
+                
+                output = self.get_channel(channel)
+                if output is None:
+                    raise RuntimeError('The channel %s on device %s is in the last programmed values, but is not in the AO, DO or DDS output store. Something has gone badly wrong!'%(channel,self.device_name))
+                else:
+                    output.set_value(remote_value,program=False)
+            
+            # Update the last_programmed_values            
+            self._last_programmed_values = self.get_front_panel_values()
     
-    @define_state(STATE_MANUAL,True)
+    @define_state(MODE_MANUAL,True)
     def check_remote_values(self):
-        results = yield(self.queue_work(self._primary_worker,'check_remote_values'))
+        self._last_remote_values = yield(self.queue_work(self._primary_worker,'check_remote_values'))
         # TODO: compare to current front panel values and prompt the user if they don't match
         
         # If no results were returned, raise an exception so that we don't keep calling this function over and over again, 
         # filling up the text box with the same error, eventually consuming all CPU/memory of the PC
-        if not results:
+        if not self._last_remote_values or type(self._last_remote_values) != type({}):
             raise Exception('Failed to get remote values from device. Is it still connected?')
             
-        current_values = 
-        # We expect a dictionary of channel:value pairs
-        for channel,remote_value in results.items():
+        # A variable to indicate if any of the channels have a changed value
+        overall_changed = False
             
+        # A place to store radio buttons in
+        self._changed_radio_buttons = {}
+            
+        # We expect a dictionary of channel:value pairs
+        for channel in sorted(self._last_remote_values):
+            remote_value = self._last_remote_values[channel]
+            if channel not in self._last_programmed_values:
+                raise RuntimeError('The worker function check_remote_values for device %s is returning data for channel %s but the BLACS tab is not programmed to handle this channel'%(self.device_name,channel))
+            
+            # A variable to indicate if this channel has changed
+            changed = False
+            
+            if channel in self._DDS:
+                # This si a complicated case!
+                #ui = QUiLoader().load('tab_value_changed_dds.ui')
+                pass
+            elif channel in self._DO:
+                # This is an easy case!
+                front_value = str(bool(int(self._last_programmed_values[channel])))
+                remote_value = str(bool(int(remote_value)))
+                if front_value != remote_value:
+                    changed = True
+                    ui = QUiLoader().load('tab_value_changed.ui')
+                    ui.channel_label.setText(self._DO.name)
+                    ui.front_value.setText(front_value)
+                    ui.remote_value.setText(remote_value)
+            elif channel in self._AO:
+                # A intermediately complicated case!
+                front_value = ("%."+str(self._AO[channel]._decimals)+"f")%self._last_programmed_values[channel]
+                remote_value = ("%."+str(self._AO[channel]._decimals)+"f")%remote_value
+                if front_value != remote_value:
+                    changed = True
+                    ui = QUiLoader().load('tab_value_changed.ui')
+                    ui.channel_label.setText(self._AO.name)
+                    ui.front_value.setText(front_value)
+                    ui.remote_value.setText(remote_value)
+            else:
+                raise RuntimeError('device_base_class.py is not programmed to handle channel types other than DDS, AO and DO in check_remote_values')
+                    
+            if changed:
+                overall_changed = True
+            
+                # Add the changed widget for this channel to a layout!
+                self._ui.changed_layout.addWidget(ui)
+                
+                # save the radio buttons so that we can access their state later!
+                self._changed_radio_buttons[channel] = ui.use_remote_values
+                
+        if overall_changed:
+            # TODO: Disable all widgets for this device, including virtual device widgets...how do I do that?????
+            # Probably need to add a disable/enable method to analog/digital/DDS widgets that disables the widget and is orthogonal to the lock/unlock system
+            # Should probably set a tooltip on the widgets too explaining why they are disabled!
+            # self._device_widget.setSensitive(False)
+            # show the remote_values_change dialog
+            self._changed_widget.show()
+        
+            # Add an "apply" button and link to on_resolve_value_inconsistency
+            button = QButton("Apply")
+            button.clicked.connect(self.on_resolve_value_inconsistency)
+            self._ui.changed_layout.addWidget(button)
+              
+    def on_resolve_value_inconsistency(self):
+        # get the values and update the device/front panel
+        needs_programming = False
+        for channel,radio in self._changed_radio_buttons.items():
+            if radio.isChecked():
+                output = self.get_channel(channel)
+                if output is None:
+                    raise RuntimeError('on_resolve_value_inconsistency is being asked to handle a channel that is not a DDS, AO or DO (channel: %s, device: %s)'%(channel,self.device_name))
+                # The device already has this value, so no need to program it!
+                output.set_value(self._last_remote_values[channel],program=False)
+            else:
+                # we only need to program the device if one or more channels is using the front panel value
+                needs_programming = True
+                
+        if needs_programming:
+            self.program_device()
+            
+        self._changed_widget.hide()
     
-    @define_state(STATE_MANUAL,True)
+    @define_state(MODE_MANUAL,True)
     def transition_to_buffered(self,h5_file,notify_queue): 
-        self.mode = STATE_TRANSITION_TO_BUFFERED
+        # Get rid of any "remote values changed" dialog
+        self._changed_widget.hide()
+    
+        self.mode = MODE_TRANSITION_TO_BUFFERED
         # The final values of the run, to update the GUI with at the end of the run:
         self._final_values = yield(self.queue_work(self._primary_worker,'transition_to_buffered',self.get_front_panel_values(),self._force_full_buffered_reprogram))
         
         # If we get None back, then the worker process did not finish properly
-        if self._final_values = None:
+        if self._final_values is None:
             notify_queue.put([self.device_name,'fail'])
             self.abort_transition_to_buffered()
         else:
             # Tell the queue manager that we're done:
-            self.mode = STATE_BUFFERED
+            self.mode = MODE_BUFFERED
             notify_queue.put([self.device_name,'success'])
        
-    @define_state(STATE_TRANSITION_TO_BUFFERED,False)
+    @define_state(MODE_TRANSITION_TO_BUFFERED,False)
     def abort_transition_to_buffered(self):
         abort_success = yield(self.queue_work(self._primary_worker,'abort_transition_to_buffered'))
         if abort:
-            self.mode = STATE_MANUAL
+            self.mode = MODE_MANUAL
             self.program_device()
         else:
             raise Exception('Could not abort transition_to_buffered. You must restart this device to continue')
         
-    @define_state(STATE_BUFFERED,False)
+    @define_state(MODE_BUFFERED,False)
     def abort_buffered(self,notify_queue):
         abort_success = yield(self.queue_work(self._primary_worker,'abort_buffered'))
         if abort_success:
             notify_queue.put([self.device_name,'success'])
-            self.mode = STATE_MANUAL
+            self.mode = MODE_MANUAL
             self.program_device()
         else:
             notify_queue.put([self.device_name,'fail'])
             raise Exception('Could not abort the buffered sequence. You must restart this device to continue')
             
-    @define_state(STATE_BUFFERED,False)
+    @define_state(MODE_BUFFERED,False)
     def transition_to_manual(self,notify_queue,program=True):
-        self.mode = STATE_TRANSITION_TO_MANUAL
+        self.mode = MODE_TRANSITION_TO_MANUAL
         
         success = yield(self.queue_work(self._primary_worker,'transition_to_static'))
         
@@ -335,7 +441,7 @@ class DeviceTab(Tab):
             
         if success:
             notify_queue.put([self.device_name,'success'])
-            self.mode = STATE_MANUAL
+            self.mode = MODE_MANUAL
         else:
             notify_queue.put([self.device_name,'fail'])
             raise Exception('Could not transition to manual. You must restart this device to continue')
