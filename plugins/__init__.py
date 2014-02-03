13849ff5da4050f51e617605164ac9646d7f361c
Phil Starkey <philipstarkey@users.noreply.github.com>
2014-02-03 16:08:44 +1100

    Fixed bug in plugins system due to rename of module BLACS to blacs (see changeset:a6384b01a699). Bumped patch version number.

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -23,7 +23,7 @@ this_dir = os.path.dirname(os.path.abspath(__file__))
 for module_name in os.listdir(this_dir):
     if os.path.isdir(os.path.join(this_dir, module_name)):
         try:
-            module = importlib.import_module('BLACS.plugins.'+module_name)
+            module = importlib.import_module('blacs.plugins.'+module_name)
         except Exception:
             logger.exception('Could not import plugin \'%s\'. Skipping.'%module_name)
         else:
