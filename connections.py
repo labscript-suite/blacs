3fbb42ced983be607d8aa1ee546d4e7a4c5a1ce1
Phil Starkey <philipstarkey@users.noreply.github.com>
2014-10-02 15:42:28 +1000

    1.1.0-dev: Expanded ConnectionTable classes
     * Now made it simpler to add columns to the connection table while maintaining backwards compatibility by introducing a wrapping class to the HDF5 row. This wrapping class returns default values if the column does not exist in the connection table.
     * Added support for a "properties" column which will be a dictionary containing properties of the device that should be compared when checking for a valid subset.

--- connections.py
+++ connections.py
@@ -33,11 +33,16 @@ class ConnectionTable(object):
                     else:
                         self.table = np.array([])
                     for row in self.table:
+                        row = Row(row)
                         if row[3] == "None":
-                            self.toplevel_children[row[0]] = Connection(row[0],row[1],None,row[3],row[4],row[5],row[6],self.table)
+                            self.toplevel_children[row[0]] = Connection(row[0],row[1],None,row[3],row[4],row[5],row[6],row[7],self.table)
                     try:
                         self.master_pseudoclock = table.attrs['master_pseudoclock']
                     except:
+                        #TODO: This will cause the Queue Manager to be unhappy
+                        #      as it needs to know the master pseudoclock
+                        #
+                        #      Perhaps we should just raise an exception here?
                         self.master_pseudoclock = None
                 except:
                     self.logger.error('Unable to get connection table  %s'%h5file)
@@ -139,10 +144,32 @@ class ConnectionTable(object):
                 if result is not None:
                     return result
         return None
+
+# A wrapper class for rows taken out of the connection table
+# This allows us to easily provide backwards compatibilty for older HDF5
+# files when we add a new column to the connection table
+#
+# If a column is missing in the connection table, the values used
+# for that column will match those specified in thge defaults dictionary
+class Row(object):
+    def __init__(self, row):
+        self.row = row
+        # key,value pairs of default values. 
+        # The key indicates the column number in the connection table (indexing 
+        #     starts from 0)
+        # The value indicates the default value to be used if the column
+        #     does not exist
+        self.defaults = {4:"None", 5:"None", 6:"", 7:"{}"}
+    def __getitem__(self, index):
+        if index >= len(self.row):
+            return self.defaults[index]
+        else:
+            return self.row[index]
+            
     
 class Connection(object):
     
-    def __init__(self, name, device_class, parent, parent_port, unit_conversion_class, unit_conversion_params, BLACS_connection, table):
+    def __init__(self, name, device_class, parent, parent_port, unit_conversion_class, unit_conversion_params, BLACS_connection, properties, table):
         self.child_list = {}
         self.name = name
         self.device_class = device_class
@@ -151,11 +178,13 @@ class Connection(object):
         self.unit_conversion_class = unit_conversion_class
         self.unit_conversion_params = unit_conversion_params
         self.BLACS_connection = BLACS_connection
+        self.properties = eval(properties)
         
         # Create children
         for row in table:
+            row = Row(row)
             if row[2] == self.name:
-                self.child_list[row[0]] = Connection(row[0],row[1],self,row[3],row[4],row[5],row[6],table)
+                self.child_list[row[0]] = Connection(row[0],row[1],self,row[3],row[4],row[5],row[6],row[7],table)
         
     def compare_to(self,other_connection):
         if not isinstance(other_connection,Connection):
@@ -175,6 +204,8 @@ class Connection(object):
             error["unit_conversion_params"] = True
         if self.BLACS_connection != other_connection.BLACS_connection:
             error["BLACS_connection"] = True
+        if self.properties != other_connection.properties:
+            error["properties"] = True
         
         # for each child in other_connection, check that the child also exists here
         for name,connection in other_connection.child_list.items():
@@ -211,6 +242,7 @@ class Connection(object):
             val = v.find_child(device_name,parent_port)
             if val is not None:
                 return val
+                
         
         return None
 
