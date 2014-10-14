724d96a0dd717d078c7d74f5b39861c40d89d1b2
Chris Billington <chrisjbillington@gmail.com>
2014-10-14 17:25:14 -0400

    Now imports check_version from labscript_utils

--- __main__.py
+++ __main__.py
@@ -54,23 +54,10 @@ else:
         from PySide.QtGui import *
 
 
-def check_version(module_name, at_least, less_than, version=None):
-
-    class VersionException(Exception):
-        pass
-
-    def get_version_tuple(version_string):
-        version_tuple = [int(v.replace('+', '-').split('-')[0]) for v in version_string.split('.')]
-        while len(version_tuple) < 3:
-            version_tuple += (0,)
-        return version_tuple
-
-    if version is None:
-        version = __import__(module_name).__version__
-    at_least_tuple, less_than_tuple, version_tuple = [get_version_tuple(v) for v in [at_least, less_than, version]]
-    if not at_least_tuple <= version_tuple < less_than_tuple:
-        raise VersionException(
-            '{module_name} {version} found. {at_least} <= {module_name} < {less_than} required.'.format(**locals()))
+try:
+    from labscript_utils import check_version
+except ImportError:
+    raise ImportError('Require labscript_utils > 2.1.0')
 
 check_version('labscript_utils', '2.0', '3')
 check_version('qtutils', '1.5.1', '2')
