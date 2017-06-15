3c641cbd378514423988247a753a1a9da7449830
Chris Billington <chrisjbillington@gmail.com>
2017-06-14 20:31:23 -0400

    feature: Make apply button for remote values check have an icon, be regular sized, and be on the left. (the previous attempt at this, backed out in the previous commit, made additional buttons by mistake).

--- device_base_class.py
+++ device_base_class.py
@@ -498,10 +498,15 @@ class DeviceTab(Tab):
             self._changed_widget.show()
         
             # Add an "apply" button and link to on_resolve_value_inconsistency
-            button = QPushButton("Apply")
+            buttonWidget = QWidget()
+            buttonlayout = QHBoxLayout(buttonWidget)
+            button = QPushButton(QIcon(':/qtutils/fugue/arrow-turn-000-left'), "Apply")
             button.clicked.connect(self.on_resolve_value_inconsistency)
-            self._ui.changed_layout.addWidget(button)
-              
+            buttonlayout.addWidget(button)
+            buttonlayout.addStretch()
+
+            self._ui.changed_layout.addWidget(buttonWidget)
+
     def on_resolve_value_inconsistency(self):
         # get the values and update the device/front panel
         needs_programming = False
