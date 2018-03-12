73236158c24dbe2d4a57a9e17509c56597c375ac
Chris Billington <chrisjbillington@gmail.com>
2018-03-13 10:20:07 +1100

    move-connections: Backwards compatibility for programs that imported blacs.connections

---
+++ connections.py
@@ -0,0 +1,17 @@
+#####################################################################
+#                                                                   #
+# /connections.py                                                   #
+#                                                                   #
+# Copyright 2013, Monash University                                 #
+#                                                                   #
+# This file is part of the program BLACS, in the labscript suite    #
+# (see http://labscriptsuite.org), and is licensed under the        #
+# Simplified BSD License. See the license.txt file in the root of   #
+# the project for the full license.                                 #
+#                                                                   #
+#####################################################################
+
+# This file solely exists as backward compatibility for programs (i.e. runviewer)
+# that imported blacs.connections, which has moved to labscript_utils.
+
+from labscript_utils.connections import *
