8f4bdaac7000e3b797dc723949c5720dc39eb92a
Jan Werkmann <jan.wrk.fb@gmail.com>
2018-06-12 09:13:59 +0200

    ConnectionTable-exceptions: Raised the version requirement for labscript_utils to 2.7.2

--- __main__.py
+++ __main__.py
@@ -45,7 +45,7 @@ try:
 except ImportError:
     raise ImportError('Require labscript_utils > 2.1.0')
 
-check_version('labscript_utils', '2.7.1', '3')
+check_version('labscript_utils', '2.7.2', '3')
 check_version('qtutils', '2.0.0', '3.0.0')
 check_version('zprocess', '1.1.2', '3')
 check_version('labscript_devices', '2.0', '3')
