6669571eb4aff06788ca1bb474cb3274e6df2859
Chris Billington <chrisjbillington@gmail.com>
2018-10-17 16:35:11 -0400

    defer_worker_start: Depend on zprocess 2.9.3, both for BLACS itself in __main__.py, and for anyone importing tab_base_classes (such as labscript_devices).

--- __main__.py
+++ __main__.py
@@ -51,7 +51,7 @@ except ImportError:
 
 check_version('labscript_utils', '2.7.2', '3')
 check_version('qtutils', '2.0.0', '3.0.0')
-check_version('zprocess', '1.1.2', '3')
+check_version('zprocess', '2.9.2', '3')
 check_version('labscript_devices', '2.0', '3')
 
 
