c6061d9957556cd346d2618907be281836fb19e0
Carter Turn <carterturn@tutanota.de>
2024-07-28 21:54:40 -0400

    First version of new virtual device system.
    Digital controls tested. Still need to add mechanism for creating virtual devices.
    Inverted digital outputs probably do not work.

---
+++ blacs/plugins/virtual_device/__init__.py
@@ -0,0 +1,123 @@
+#####################################################################
+#                                                                   #
+# /plugins/virtual_device/__init__.py                               #
+#                                                                   #
+# Copyright 2024, Carter Turnbaugh                                  #
+#                                                                   #
+#####################################################################
+import logging
+import os
+import subprocess
+import threading
+import sys
+import time
+
+from qtutils import inmain, inmain_decorator
+
+import labscript_utils.h5_lock
+import h5py
+
+import labscript_utils.properties as properties
+from labscript_utils.connections import ConnectionTable
+from zprocess import TimeoutError
+from labscript_utils.ls_zprocess import Event
+from blacs.plugins import PLUGINS_DIR, callback
+
+from .virtual_device_tab import VirtualDeviceTab
+
+name = "Virtual Device"
+module = "virtual_device" # should be folder name
+logger = logging.getLogger('BLACS.plugin.%s'%module)
+
+# Try to reconnect often in case a tab restarts
+CONNECT_CHECK_INTERVAL = 0.1
+
+class Plugin(object):
+    def __init__(self, initial_settings):
+        self.menu = None
+        self.notifications = {}
+        self.initial_settings = initial_settings
+        self.BLACS = None
+        self.disconnected_last = False
+
+        self.virtual_devices = initial_settings.get('virtual_devices', {})
+
+        self.tab_dict = {}
+
+        self.setup_complete = False
+        self.close_event = threading.Event()
+        self.reconnect_thread = threading.Thread(target=self.reconnect, args=(self.close_event,))
+        self.reconnect_thread.daemon = True
+
+        self.tab_restart_receiver = lambda dn, s=self: self.disconnect_widgets(dn)
+
+    @inmain_decorator(True)
+    def connect_widgets(self):
+        if not self.setup_complete:
+            return
+        for name, vd_tab in self.tab_dict.items():
+            vd_tab.connect_widgets()
+        for _, tab in self.BLACS['ui'].blacs.tablist.items():
+            if hasattr(tab, 'connect_restart_receiver'):
+                tab.connect_restart_receiver(self.tab_restart_receiver)
+
+    @inmain_decorator(True)
+    def disconnect_widgets(self, closing_device_name):
+        if not self.setup_complete:
+            return
+        self.BLACS['ui'].blacs.tablist[closing_device_name].disconnect_restart_receiver(self.tab_restart_receiver)
+        for name, vd_tab in self.tab_dict.items():
+            vd_tab.disconnect_widgets(closing_device_name)
+
+    def reconnect(self, stop_event):
+        while not stop_event.wait(CONNECT_CHECK_INTERVAL):
+            self.connect_widgets()
+
+    def on_tab_layout_change(self):
+        return
+
+    def plugin_setup_complete(self, BLACS):
+        self.BLACS = BLACS
+
+        for name, vd_tab in self.tab_dict.items():
+            vd_tab.create_widgets(self.BLACS['ui'].blacs.tablist,
+                                  self.virtual_devices[name]['AO'],
+                                  self.virtual_devices[name]['DO'],
+                                  self.virtual_devices[name]['DDS'])
+
+        self.setup_complete = True
+        self.reconnect_thread.start()
+
+    def get_save_data(self):
+        return {'virtual_devices': {
+            'v0': {'AO': [], 'DO': [('christopher', 'GPIO 09'), ('pdo_0', '0x1')], 'DDS': []},
+            'v1': {'AO': [], 'DO': [('pdo_0', '0x0'), ('pdo_0', '0x2')], 'DDS': []},
+        }}
+
+    def get_tab_classes(self):
+        return {k: VirtualDeviceTab for k in self.virtual_devices.keys()}
+
+    def tabs_created(self, tab_dict):
+        self.tab_dict = tab_dict
+
+    def get_callbacks(self):
+        return {}
+
+    def get_menu_class(self):
+        return None
+
+    def get_notification_classes(self):
+        return []
+
+    def get_setting_classes(self):
+        return []
+
+    def set_menu_instance(self, menu):
+        self.menu = menu
+
+    def set_notification_instances(self, notifications):
+        self.notifications = notifications
+
+    def close(self):
+        self.close_event.set()
+        self.reconnect_thread.join()
---
+++ blacs/plugins/virtual_device/virtual_device_tab.py
@@ -0,0 +1,71 @@
+from qtutils.qt.QtCore import *
+from qtutils.qt.QtGui import *
+from qtutils.qt.QtWidgets import *
+
+from labscript_utils.qtwidgets.toolpalette import ToolPaletteGroup
+
+from blacs.tab_base_classes import PluginTab
+
+class VirtualDeviceTab(PluginTab):
+
+    def create_widgets(self, blacs_tablist, AOs, DOs, DDSs):
+        self._blacs_tablist = blacs_tablist
+        self._AOs = {(AO[0], AO[1]): None for AO in AOs}
+        self._DOs = {(DO[0], DO[1]): None for DO in DOs}
+        self._DDSs = {(DDS[0], DDS[1]): None for DDS in DDSs}
+
+        for AO in self._AOs.keys():
+            if self._AOs[AO] is None:
+                self._AOs[AO] = self._blacs_tablist[AO[0]]._AO[AO[1]].create_widget(None, False, None)
+                self._AOs[AO].last_AO = None
+
+        for DO in self._DOs.keys():
+            if self._DOs[DO] is None:
+                self._DOs[DO] = self._blacs_tablist[DO[0]]._DO[DO[1]].create_widget()
+                self._DOs[DO].last_DO = None
+
+        dds_widgets = []
+
+        if len(self._AOs) > 0:
+            self.place_widget_group('Analog Outputs', [v for k, v in self._AOs.items()])
+        if len(self._DOs) > 0:
+            self.place_widget_group('Digital Outputs', [v for k, v in self._DOs.items()])
+
+        return
+
+    def connect_widgets(self):
+        for AO in self._AOs.keys():
+            if self._AOs[AO] is not None:
+                new_AO = self._blacs_tablist[AO[0]]._AO[AO[1]]
+                if self._AOs[AO].get_AO() is None and self._AOs[AO].last_AO != new_AO:
+                    self._AOs[AO].set_AO(new_AO)
+        for DO in self._DOs.keys():
+            if self._DOs[DO] is not None:
+                new_DO = self._blacs_tablist[DO[0]]._DO[DO[1]]
+                if self._DOs[DO].get_DO() is None and self._DOs[DO].last_DO != new_DO:
+                    self._DOs[DO].set_DO(new_DO)
+
+    def disconnect_widgets(self, closing_device_name):
+        for AO in self._AOs.keys():
+            if AO[0] == closing_device_name:
+                self._AOs[AO].last_AO = self._AOs[AO].get_AO()
+                self._AOs[AO].set_AO(None)
+        for DO in self._DOs.keys():
+            if DO[0] == closing_device_name:
+                self._DOs[DO].last_DO = self._DOs[DO].get_DO()
+                self._DOs[DO].set_DO(None)
+
+    def place_widget_group(self, name, widgets):
+        widget = QWidget()
+        toolpalettegroup = ToolPaletteGroup(widget)
+
+        if toolpalettegroup.has_palette(name):
+            toolpalette = toolpalettegroup.get_palette(name)
+        else:
+            toolpalette = toolpalettegroup.append_new_palette(name)
+
+        for output_widget in widgets:
+            toolpalette.addWidget(output_widget, True)
+
+        self.get_tab_layout().addWidget(widget)
+        self.get_tab_layout().addItem(QSpacerItem(0,0,QSizePolicy.Minimum,QSizePolicy.MinimumExpanding))
